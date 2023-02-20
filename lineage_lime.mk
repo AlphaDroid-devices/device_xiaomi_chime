92c98ecb30d6fc514ed70f9b1eb05633726a656a
Drpto <dr.juniornoob@gmail.com>
2023-02-19 18:06:39 -0800

    chime: BringUp for AlphaDroid
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- lineage_lime.mk
+++ lineage_lime.mk
@@ -10,6 +10,23 @@ $(call inherit-product, device/xiaomi/chime/device-lime.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# GAPPS flag
+WITH_GAPPS := true
+TARGET_CORE_GAPPS := true
+
+# AlphaDroid Build Tags
+ALPHA_BUILD_TYPE := Unofficial
+ALPHA_MAINTAINER := DrOpto
+
+#Supported Device Flags (Features)
+TARGET_ENABLE_BLUR := true
+TARGET_INCLUDE_MATLOG := false
+TARGET_USE_GRAPHENE_CAMERA := true
+TARGET_USE_PIXEL_LAUNCHER := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_lime
 PRODUCT_DEVICE := lime
